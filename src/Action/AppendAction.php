7fc4f79c2d061edb198a805eed449ff162455081
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-10-03 15:01:39 -0400

    Funcionan todos los tests

---
+++ src/Action/AppendAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Andaniel05\GluePHP\Action;
+
+use Andaniel05\GluePHP\Action\AbstractAction;
+use Andaniel05\GluePHP\Component\AbstractComponent;
+
+class AppendAction extends AbstractAction
+{
+    public function __construct(AbstractComponent $parent, AbstractComponent $child)
+    {
+        parent::__construct([
+            'parentId' => $parent->getId(),
+            'childId'  => $child->getId(),
+            'html'     => $child->html(),
+        ]);
+    }
+
+    public static function handlerScript(): string
+    {
+        return <<<JAVASCRIPT
+    var parent = app.getComponent(data.parentId);
+
+    if (parent instanceof GluePHP.Component &&
+        parent.html instanceof Element)
+    {
+        var childElement = document.createElement('div');
+        childElement.innerHTML = data.html;
+        childElement = childElement.firstChild;
+
+        parent.html.append(childElement);
+    }
+
+JAVASCRIPT;
+    }
+}
--- tests/Integration/Append/apps/base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Andaniel05\GluePHP\Tests\TestApp;
-use Andaniel05\GluePHP\Tests\Integration\Entities\Components\Button;
-
-$button1 = new Button('button1');
-
-$app = new TestApp();
-$app->appendComponent('body', $button1);
