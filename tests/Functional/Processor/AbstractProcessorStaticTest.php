56405b353723fca51e8eeb6d5296d42659598d3f
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-10-19 08:27:14 -0400

    Se ha testeado la funcion traverseElements

--- tests/Functional/Processor/AbstractProcessorStaticTest.php
+++ tests/Functional/Processor/AbstractProcessorStaticTest.php
@@ -8,31 +8,104 @@ use Andaniel05\GluePHP\Processor\AbstractProcessor;
 
 class AbstractProcessorStaticTest extends StaticTestCase
 {
-    public function test1()
+    public function provider()
+    {
+        return [
+            [
+                $attr = uniqid('data-attr'),
+                '<div></div>',
+                false,
+                "<div {$attr}=\"\"></div>",
+            ],
+            [
+                $attr = uniqid('data-attr'),
+                '<div>
+                    <div></div>
+                </div>',
+                false,
+                "<div {$attr}=\"\">
+                    <div {$attr}=\"\"></div>
+                </div>",
+            ],
+            [
+                $attr = uniqid('data-attr'),
+                '<div>
+                    <div></div>
+                    <div></div>
+                </div>',
+                false,
+                "<div {$attr}=\"\">
+                    <div {$attr}=\"\"></div>
+                    <div {$attr}=\"\"></div>
+                </div>",
+            ],
+            [
+                $attr = uniqid('data-attr'),
+                '<div>
+                    <div class="gphp-children gphp-component-children">
+                        <div></div>
+                    </div>
+                </div>',
+                false,
+                "<div {$attr}=\"\">
+                    <div class=\"gphp-children gphp-component-children\">
+                        <div></div>
+                    </div>
+                </div>",
+            ],
+            [
+                $attr = uniqid('data-attr'),
+                '<div>
+                    <div class="gphp-children gphp-component-children">
+                        <div></div>
+                    </div>
+                </div>',
+                true,
+                "<div {$attr}=\"\">
+                    <div {$attr}=\"\" class=\"gphp-children gphp-component-children\">
+                        <div {$attr}=\"\"></div>
+                    </div>
+                </div>",
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testTraverseElements($attr, $html, $includeChildren, $expected)
     {
         $processor = new class extends AbstractProcessor {
 
+            public static $attr;
+            public static $includeChildren;
+
             public static function script(): string
             {
+                $attr = static::$attr;
+                $includeChildren = static::$includeChildren;
+
                 return <<<JAVASCRIPT
     traverseElements(function(element) {
-        var attr = document.createAttribute('data-attr');
-        attr.value = '';
+        var attr = document.createAttribute("{$attr}");
         element.setAttributeNode(attr);
-    });
+    }, {$includeChildren});
 JAVASCRIPT;
             }
         };
 
         $processorClass = get_class($processor);
+        $processorClass::$attr = $attr;
+        $processorClass::$includeChildren = $includeChildren;
 
-        $component = new class('component', $processorClass) extends AbstractComponent {
+        $component = new class('component', $processorClass, $html) extends AbstractComponent {
 
-            public function __construct($id, $processorClass)
+            public function __construct($id, $processorClass, $html)
             {
                 parent::__construct($id);
 
                 $this->processorClass = $processorClass;
+                $this->html = $html;
             }
 
             public function processors(): array
@@ -42,7 +115,7 @@ JAVASCRIPT;
 
             public function html(): ?string
             {
-                return '<div></div>';
+                return $this->html;
             }
         };
 
@@ -50,7 +123,7 @@ JAVASCRIPT;
         $this->writeDocument($this->app->html());
 
         $this->assertXmlStringEqualsXmlString(
-            "<div data-attr=\"\"></div>",
+            $expected,
             $this->script("return app.components['component'].element.innerHTML")
         );
     }
