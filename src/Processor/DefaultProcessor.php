4730be00a060c44c41968d6585c1a23817ad3989
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-11-19 16:02:22 -0400

    Temp

--- src/Processor/DefaultProcessor.php
+++ src/Processor/DefaultProcessor.php
@@ -62,6 +62,61 @@ class DefaultProcessor extends AbstractProcessor
         });
     };
 
+    /////////////////////
+    // Bind Attributes //
+    /////////////////////
+
+    if (component.element instanceof Element) {
+        bindAttributes('gphp-bind-attr-');
+        bindAttributes('data-gphp-bind-attr-');
+    }
+
+    function bindAttributes(gphpAttr) {
+        traverseElements(function(child) {
+
+            var atts = child.getAttributeNames();
+            atts.forEach(function(attr) {
+
+                if (0 === attr.indexOf(gphpAttr)) {
+
+                    var gAttr = child.getAttribute(attr),
+                        htmlAttr = attr.substr(gphpAttr.length);
+
+                    child.setAttribute(htmlAttr, component.model[gAttr]);
+
+                    var setterName = GluePHP.Helpers.getSetter(gAttr);
+                    var oldSetter = component[setterName];
+
+                    component[setterName] = function(value, registerUpdate = true) {
+                        oldSetter.call(this, value, registerUpdate);
+                        child.setAttribute(htmlAttr, value);
+                    }
+
+                    var observer = new MutationObserver(function(mutations) {
+                        mutations.forEach(function(mutation) {
+                            if ('attributes' === mutation.type &&
+                                htmlAttr == mutation.attributeName)
+                            {
+                                var newVal = child.getAttribute(htmlAttr);
+                                component[setterName](newVal);
+                            }
+                        });
+                    });
+
+                    var config = {
+                        attributes: true,
+                        // attributeOldValue: true,
+                        attributeFilter: [htmlAttr]
+                    };
+
+                    observer.observe(child, config);
+                }
+
+            });
+
+        });
+    };
+
 JAVASCRIPT;
     }
 }
--- tests/Functional/Processor/DefaultProcessorStaticTest.php
+++ tests/Functional/Processor/DefaultProcessorStaticTest.php
@@ -7,7 +7,7 @@ use Andaniel05\GluePHP\Tests\StaticTestCase;
 
 class DefaultProcessorStaticTest extends StaticTestCase
 {
-    public function providerGPhpEventAttribute()
+    public function providerGPhpEvent()
     {
         return [
             ['gphp-event'], ['data-gphp-event']
@@ -15,7 +15,7 @@ class DefaultProcessorStaticTest extends StaticTestCase
     }
 
     /**
-     * @dataProvider providerGPhpEventAttribute
+     * @dataProvider providerGPhpEvent
      */
     public function testGPhpEventAttribute_BindOneEventToTheComponent($attribute)
     {
@@ -60,7 +60,7 @@ JAVASCRIPT;
     }
 
     /**
-     * @dataProvider providerGPhpEventAttribute
+     * @dataProvider providerGPhpEvent
      */
     public function testGPhpEventAttribute_BindSeveralEventsToTheComponent($attribute)
     {
@@ -139,7 +139,7 @@ JAVASCRIPT;
         $this->driver->switchTo()->alert()->accept();
     }
 
-    public function providerGBindAttribute()
+    public function providerGPhpBindValue()
     {
         return [
             ['gphp-bind-value'], ['data-gphp-bind-value']
@@ -147,9 +147,9 @@ JAVASCRIPT;
     }
 
     /**
-     * @dataProvider providerGBindAttribute
+     * @dataProvider providerGPhpBindValue
      */
-    public function testGBindAttribute_ViewToComponent($attribute)
+    public function testGPhpBindValue_ViewToComponent($attribute)
     {
         $component1 = new class($attribute) extends AbstractComponent {
 
@@ -194,9 +194,9 @@ JAVASCRIPT;
     }
 
     /**
-     * @dataProvider providerGBindAttribute
+     * @dataProvider providerGPhpBindValue
      */
-    public function testGBindAttribute_ComponentToView_TheUpdateIsRegistered($attribute)
+    public function testGPhpBindValue_ComponentToView_TheUpdateIsRegistered($attribute)
     {
         $component1 = new class($attribute) extends AbstractComponent {
 
@@ -235,6 +235,133 @@ JAVASCRIPT;
         $this->assertEquals(
             $secret,
             $this->script("return app.buffer.component1.text")
+        );
+    }
+
+    public function providerGPhpBindAttr()
+    {
+        return [
+            ['gphp-bind-attr'], ['data-gphp-bind-attr']
+        ];
+    }
+
+    /**
+     * @dataProvider providerGPhpBindAttr
+     */
+    public function testBoundsAttributesAreInitialized($gphpBindAttr)
+    {
+        $htmlAttrToBind = uniqid('attr');
+
+        $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($gphpBindAttr, $htmlAttrToBind)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $secret = uniqid();
+        $component1->setGAttr($secret);
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').getAttribute('{$htmlAttrToBind}');")
+        );
+    }
+
+    /**
+     * @dataProvider providerGPhpBindAttr
+     */
+    public function testAttributeBindingFromTheComponentModelToTheView($gphpBindAttr)
+    {
+        $htmlAttrToBind = uniqid('attr');
+
+        $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($gphpBindAttr, $htmlAttrToBind)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $secret = uniqid();
+        $this->script("app.getComponent('component1').setGAttr('{$secret}')");
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').getAttribute('{$htmlAttrToBind}');")
+        );
+    }
+
+    /**
+     * @dataProvider providerGPhpBindAttr
+     */
+    public function testAttributeBindingFromTheViewToTheComponentModel($gphpBindAttr)
+    {
+        $htmlAttrToBind = uniqid('attr');
+
+        $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($gphpBindAttr, $htmlAttrToBind)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $secret = uniqid();
+        $this->script("document.getElementById('div').setAttribute('{$htmlAttrToBind}', '{$secret}');");
+
+        $this->assertEquals(
+            $secret, $this->script("return app.getComponent('component1').model.gAttr;")
         );
     }
 }
