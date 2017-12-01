5e914cafd734e662d01def422a9b4b81e2a0c557
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-12-01 10:42:31 -0400

    Temp

--- src/Processor/VueProcessor.php
+++ src/Processor/VueProcessor.php
@@ -2,8 +2,17 @@
 
 namespace Andaniel05\GluePHP\Processor;
 
+use Andaniel05\ComposedViews\Asset\ScriptAsset;
+
 class VueProcessor extends AbstractProcessor
 {
+    public static function assets(): array
+    {
+        return [
+            'vuejs' => new ScriptAsset('vuejs', '')
+        ];
+    }
+
     public static function script(): string
     {
         return '';
--- tests/Unit/Processor/VueProcessorTest.php
+++ tests/Unit/Processor/VueProcessorTest.php
@@ -4,7 +4,15 @@ namespace Andaniel05\GluePHP\Tests\Unit\Processor;
 
 use PHPUnit\Framework\TestCase;
 use Andaniel05\GluePHP\Processor\VueProcessor;
+use Andaniel05\ComposedViews\Asset\ScriptAsset;
 
 class VueProcessorTest extends TestCase
 {
+    public function testDependsOfVueScript()
+    {
+        $vuejs = VueProcessor::assets()['vuejs'];
+
+        $this->assertEquals('vuejs', $vuejs->getId());
+        $this->assertInstanceOf(ScriptAsset::class, $vuejs);
+    }
 }
