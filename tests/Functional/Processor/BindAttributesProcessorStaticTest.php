3900facc71b9615e9537c2db9798600fb06bbbe8
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-12-03 11:20:38 +0100

    Se han separado los tests de DefaultProcessor

---
+++ tests/Functional/Processor/BindAttributesProcessorStaticTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Andaniel05\GluePHP\Tests\Functional\Processor;
+
+use Andaniel05\GluePHP\Component\AbstractComponent;
+use Andaniel05\GluePHP\Tests\StaticTestCase;
+
+class BindAttributesProcessorStaticTest extends StaticTestCase
+{
+    public function providerGPhpBindAttr()
+    {
+        return [
+            ['gphp-bind-attr'], ['data-gphp-bind-attr']
+        ];
+    }
+
+    /**
+     * @dataProvider providerGPhpBindAttr
+     */
+    public function testBoundsAttributesAreInitialized($gphpBindAttr)
+    {
+        $htmlAttrToBind = uniqid('attr');
+
+        $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($gphpBindAttr, $htmlAttrToBind)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $secret = uniqid();
+        $component1->setGAttr($secret);
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').getAttribute('{$htmlAttrToBind}');")
+        );
+    }
+
+    /**
+     * @dataProvider providerGPhpBindAttr
+     */
+    public function testAttributeBindingFromTheComponentModelToTheView($gphpBindAttr)
+    {
+        $htmlAttrToBind = uniqid('attr');
+
+        $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($gphpBindAttr, $htmlAttrToBind)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $secret = uniqid();
+        $this->script("app.getComponent('component1').setGAttr('{$secret}')");
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').getAttribute('{$htmlAttrToBind}');")
+        );
+    }
+
+    // /**
+    //  * @dataProvider providerGPhpBindAttr
+    //  */
+    // public function testAttributeBindingFromTheViewToTheComponentModel($gphpBindAttr)
+    // {
+    //     $htmlAttrToBind = uniqid('attr');
+
+    //     $component1 = new class($gphpBindAttr, $htmlAttrToBind) extends AbstractComponent {
+
+    //         /**
+    //          * @Glue
+    //          */
+    //         protected $gAttr;
+
+    //         protected $attribute;
+
+    //         public function __construct($gphpBindAttr, $htmlAttrToBind)
+    //         {
+    //             parent::__construct('component1');
+
+    //             $this->attribute = $gphpBindAttr.'-'.$htmlAttrToBind;
+    //         }
+
+    //         public function html(): ?string
+    //         {
+    //             return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+    //         }
+    //     };
+
+    //     $this->body->addChild($component1);
+    //     $this->writeDocument($this->app->html());
+
+    //     $secret = uniqid();
+    //     $this->script("document.getElementById('div').setAttribute('{$htmlAttrToBind}', '{$secret}');");
+
+    //     $this->assertEquals(
+    //         $secret, $this->script("return app.getComponent('component1').model.gAttr;")
+    //     );
+    // }
+}
---
+++ tests/Functional/Processor/BindHtmlProcessorStaticTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Andaniel05\GluePHP\Tests\Functional\Processor;
+
+use Andaniel05\GluePHP\Component\AbstractComponent;
+use Andaniel05\GluePHP\Tests\StaticTestCase;
+
+class BindHtmlProcessorStaticTest extends StaticTestCase
+{
+    public function providerGPhpBindHtml()
+    {
+        return [
+            ['gphp-bind-html'], ['data-gphp-bind-html']
+        ];
+    }
+
+    /**
+     * @dataProvider providerGPhpBindHtml
+     */
+    public function testHtmlBindingInitializeInnerHTMLOnBoundChilds($attribute)
+    {
+        $component1 = new class($attribute) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($attribute)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $attribute;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $secret = uniqid();
+        $component1->setGAttr($secret);
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').innerHTML;")
+        );
+    }
+
+    /**
+     * @dataProvider providerGPhpBindHtml
+     */
+    public function testHtmlBindingFromTheComponentModelToTheView($attribute)
+    {
+        $component1 = new class($attribute) extends AbstractComponent {
+
+            /**
+             * @Glue
+             */
+            protected $gAttr;
+
+            protected $attribute;
+
+            public function __construct($attribute)
+            {
+                parent::__construct('component1');
+
+                $this->attribute = $attribute;
+            }
+
+            public function html(): ?string
+            {
+                return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+            }
+        };
+
+        $this->body->addChild($component1);
+        $this->writeDocument($this->app->html());
+
+        $secret = uniqid();
+        $this->script("app.getComponent('component1').setGAttr('{$secret}')");
+
+        $this->assertEquals(
+            $secret, $this->script("return document.getElementById('div').innerHTML;")
+        );
+    }
+
+    // /**
+    //  * @dataProvider providerGPhpBindHtml
+    //  */
+    // public function testHtmlBindingFromTheViewToTheComponentModel($attribute)
+    // {
+    //     $component1 = new class($attribute) extends AbstractComponent {
+
+    //         /**
+    //          * @Glue
+    //          */
+    //         protected $gAttr;
+
+    //         protected $attribute;
+
+    //         public function __construct($attribute)
+    //         {
+    //             parent::__construct('component1');
+
+    //             $this->attribute = $attribute;
+    //         }
+
+    //         public function html(): ?string
+    //         {
+    //             return "<div id=\"div\" {$this->attribute}=\"gAttr\"></div>";
+    //         }
+    //     };
+
+    //     $this->body->addChild($component1);
+    //     $this->writeDocument($this->app->html());
+
+    //     $secret = uniqid();
+    //     $this->script("document.getElementById('div').innerHTML = '{$secret}'");
+
+    //     $this->assertEquals(
+    //         $secret, $this->script("return app.getComponent('component1').model.gAttr;")
+    //     );
+    // }
+}
